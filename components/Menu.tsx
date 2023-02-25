8bd62dd836a678d58e0e80de55959915ce7a0ce8
Kashif Umar <Kashif.TLB@gmail.com>
2023-02-25 22:49:02 +0500

    Menu Part1

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,5 +1,19 @@
-const Menu = () => {
-  return <></>;
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+interface MenuItem {
+  label: string;
+  link: string;
+  subMenu?: MenuItem[];
+}
+
+interface MenuProps {
+  logo: string;
+  menuItems: MenuItem[];
+}
+
+const Menu: React.FC<MenuProps> = ({ logo, menuItems }) => {
+  return <Box></Box>;
 };
 
 export default Menu;
